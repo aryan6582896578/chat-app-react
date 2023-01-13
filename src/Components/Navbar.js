38005d53176a55ea6a0b48e45d2398e2f884b612
tbot <tbot1738@gmail.com>
2023-01-13 23:17:50 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router, Link, Route } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </Router>
+    </>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Clock from "./Components/Clock";
+import reportWebVitals from "./reportWebVitals";
+import Welcome from "./Components/WelcomeDialog";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Clock />
+    <Welcome />
   </React.StrictMode>
 );
 
