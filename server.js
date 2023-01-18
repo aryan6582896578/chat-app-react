fbb75392146803c9788d84d70d51b8f18eb775be
tbot <tbot1738@gmail.com>
2023-01-18 19:23:59 +0530

    You can now manually Add your name

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
-var rand = function () {
-  return Math.random().toString(36).substr(2); // remove `0.`
-};
+export default function Server() {
+  app.use(cors());
+  var rand = function () {
+    return Math.random().toString(36).substr(2); // remove `0.`
+  };
 
-app.use("/login", (req, res) => {
-  const token = rand();
-  res.send({
-    token,
+  app.use("/login", (req, res) => {
+    const token = rand();
+    res.send({
+      token,
+    });
   });
-});
 
-app.listen(8080, () =>
-  console.log("API is running on http://localhost:8080/login")
-);
+  app.listen(8080, () =>
+    console.log("API is running on http://localhost:8080/login")
+  );
+}
--- src/Components/Basic/Welcome.js
+++ src/Components/Basic/Welcome.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 export class Welcome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: " ",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+    if (event.target.value.length > 20) {
+      this.setState({ name: "" });
+      alert("Maximum Length Exceeded");
+    }
+  }
   render() {
     return (
       <>
-        <h1>Welcome {this.props.username}</h1>
+        <h1>Welcome {this.state.name}</h1>
+        <div className="bottom">
+          <form>
+            <input
+              type="text"
+              placeholder="Your Name here"
+              onChange={this.handleChange}
+            ></input>
+          </form>
+        </div>
       </>
     );
   }
